0bce43dd2d999b4830d8e6867b2fe76f3308f76d
Leonardo <leopaes7@outlook.com>
2020-01-21 17:04:20 -0300

    Corrigido algun erros

--- estudoPython/main.py
+++ estudoPython/main.py
@@ -2,7 +2,8 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source;
+with sr.Microphone() as source:
     print('Say something')
     audio = r.listen(source)
     voice_data = r.recognize_google(audio)
+    print(voice_data)
